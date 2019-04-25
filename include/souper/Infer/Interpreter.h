pranavk/souper

Improvements to Dataflow pruning
* Separate concrete interpreter class that caches results

The concrete interpreter only caches results during construction; we
construct a interpreter object with a LHS for each input. The object
(with cached results) is then used for future concrete interpretation in
Abstract interpreters. This should increase performance as all those
RHSes with instruction directly used from LHS have their result already
in cache.

* Prune phi instructions by absract-interpreting LHS

Contributed by Manasij here:
https://github.com/google/souper/pull/525

* Simple transfer functions for phi

* more precise implementation of constant range's binaryOr/binaryAnd

* fix phi, select related crashes when compiling gzip

* Add cache to KB and CR

* Improvements in Abstract transfer functions

* if arg0 of select is poison, result is poison - change test for it

* Use kb and cr metadata in abstract interpreters

* Refactor PruningManager to use SynthesisContext

* validate input for PC checks

include/souper/Infer/Interpreter.h
@@ -37,6 +37,7 @@ struct EvalValue {
   bool hasValue() {
     return K == ValueKind::Val;
   }
+
   llvm::APInt getValue() {
     if (K != ValueKind::Val) {
       llvm::errs() << "Interpreter: expected number but got ";
@@ -80,30 +81,39 @@ struct EvalValue {
     }
   }
 };
+
 using ValueCache = std::unordered_map<souper::Inst *, EvalValue>;
 
-EvalValue evaluateAddNSW(llvm::APInt a, llvm::APInt b);
-EvalValue evaluateAddNUW(llvm::APInt a, llvm::APInt b);
-EvalValue evaluateAddNW(llvm::APInt a, llvm::APInt b);
-EvalValue evaluateSubNSW(llvm::APInt a, llvm::APInt b);
-EvalValue evaluateSubNUW(llvm::APInt a, llvm::APInt b);
-EvalValue evaluateSubNW(llvm::APInt a, llvm::APInt b);
-EvalValue evaluateUDiv(llvm::APInt a, llvm::APInt b);
-EvalValue evaluateURem(llvm::APInt a, llvm::APInt b);
-EvalValue evaluateShl(llvm::APInt a, llvm::APInt b);
-EvalValue evaluateLShr(llvm::APInt a, llvm::APInt b);
-EvalValue evaluateAShr(llvm::APInt a, llvm::APInt b);
+EvalValue evaluateAddNSW(llvm::APInt A, llvm::APInt B);
+EvalValue evaluateAddNUW(llvm::APInt A, llvm::APInt B);
+EvalValue evaluateAddNW(llvm::APInt A, llvm::APInt B);
+EvalValue evaluateSubNSW(llvm::APInt A, llvm::APInt B);
+EvalValue evaluateSubNUW(llvm::APInt A, llvm::APInt B);
+EvalValue evaluateSubNW(llvm::APInt A, llvm::APInt B);
+EvalValue evaluateUDiv(llvm::APInt A, llvm::APInt B);
+EvalValue evaluateURem(llvm::APInt A, llvm::APInt B);
+EvalValue evaluateShl(llvm::APInt A, llvm::APInt B);
+EvalValue evaluateLShr(llvm::APInt A, llvm::APInt B);
+EvalValue evaluateAShr(llvm::APInt A, llvm::APInt B);
 
-EvalValue evaluateInst(Inst* Root, ValueCache &Cache);
-llvm::KnownBits findKnownBits(Inst* I, ValueCache& C, bool PartialEval = true);
-llvm::KnownBits findKnownBitsUsingSolver(Inst *I, Solver *S, std::vector<InstMapping> &PCs);
-llvm::ConstantRange findConstantRange(souper::Inst* I,
-                                      souper::ValueCache& C,
-                                      bool PartialEval = true);
-llvm::ConstantRange findConstantRangeUsingSolver(souper::Inst* I, Solver *S, std::vector<InstMapping> &PCs);
-bool isConcrete(souper::Inst *I, bool ConsiderConsts = true,
-                                 bool ConsiderHoles = true);
-std::string knownBitsString(llvm::KnownBits KB);
+  class ConcreteInterpreter {
+    ValueCache _Cache;
+    bool _CacheWritable = false;
+
+    EvalValue evaluateSingleInst(Inst *I, std::vector<EvalValue> &Args);
+
+  public:
+    ConcreteInterpreter() {}
+    ConcreteInterpreter(ValueCache &Input) : _Cache(Input) {}
+    ConcreteInterpreter(Inst *I, ValueCache &Input) : _Cache(Input) {
+      _CacheWritable = true;
+      evaluateInst(I);
+      _CacheWritable = false;
+    }
+
+    EvalValue evaluateInst(Inst *Root);
+  };
+
 }
 
 

lib/Infer/Interpreter.cpp
@@ -15,10 +15,6 @@
 #include "souper/Infer/Interpreter.h"
 
 namespace souper {
-#define ARG0 Args[0].getValue()
-#define ARG1 Args[1].getValue()
-#define ARG2 Args[2].getValue()
-
   EvalValue evaluateAddNSW(llvm::APInt a, llvm::APInt b) {
     bool Ov;
     auto Res = a.sadd_ov(b, Ov);
@@ -105,14 +101,18 @@ namespace souper {
     return {a.ashr(b)};
   }
 
-  EvalValue evaluateSingleInst(Inst *Inst, std::vector<EvalValue> &Args) {
+#define ARG0 Args[0].getValue()
+#define ARG1 Args[1].getValue()
+#define ARG2 Args[2].getValue()
+
+  EvalValue ConcreteInterpreter::evaluateSingleInst(Inst *Inst, std::vector<EvalValue> &Args) {
     // UB propagates unconditionally
     for (auto &A : Args)
       if (A.K == EvalValue::ValueKind::UB)
         return EvalValue::ub();
 
     // phi and select only take poison from their chosen input
-    if (Inst->K != Inst::Phi && Inst->K != Inst::Select) {
+    if (Inst->K != Inst::Select) {
       for (auto &A : Args)
         if (A.K == EvalValue::ValueKind::Poison)
           return EvalValue::poison();
@@ -134,7 +134,13 @@ namespace souper {
 
     case Inst::Phi:
       // FIXME should return all values not just one of them
-      return ARG0;
+      // At this moment, the only situation where evaluateInst is called on an
+      // Instcontaining Phi is for the purpose of partial evaluation during
+      // abstract interpretation. In this case, it is okay to return one of the
+      // operands. If we ever want to deterministically interpret an LHS
+      // containing a phi, this needs to start returning a list, or there needs
+      // to be enough information in BlockPCs to interpret ARG0
+      return Args[0];
 
     case Inst::Add:
       return {ARG0 + ARG1};
@@ -287,7 +293,7 @@ namespace souper {
 
     case Inst::Select:
       if (!Args[0].hasValue())
-        return EvalValue::ub();
+        return Args[0];
       return ARG0.getBoolValue() ? Args[1] : Args[2];
 
     case Inst::ZExt:
@@ -375,19 +381,17 @@ namespace souper {
 #undef ARG1
 #undef ARG2
 
-  EvalValue evaluateInst(Inst *Root, ValueCache &Cache) {
-    // TODO populate cache and look things up in it
-    // needed?
-    if (Root->K == Inst::Var) {
-      return Cache[Root];
-    } else {
-      // TODO SmallVector
-      std::vector<EvalValue> EvaluatedArgs;
-      for (auto &&I : Root->Ops)
-        EvaluatedArgs.push_back(evaluateInst(I, Cache));
-      auto Result = evaluateSingleInst(Root, EvaluatedArgs);
-      //Cache[Root] = Result;
-      return Result;
-    }
+  EvalValue ConcreteInterpreter::evaluateInst(Inst *Root) {
+    if (_Cache.find(Root) != _Cache.end())
+      return _Cache[Root];
+
+    // TODO SmallVector
+    std::vector<EvalValue> EvaluatedArgs;
+    for (auto &&I : Root->Ops)
+      EvaluatedArgs.push_back(evaluateInst(I));
+    auto Result = evaluateSingleInst(Root, EvaluatedArgs);
+    if (_CacheWritable)
+      _Cache[Root] = Result;
+    return Result;
   }
 }

prosyslab/hydra

include/souper/Infer/Interpreter.h
@@ -37,6 +37,7 @@ struct EvalValue {
   bool hasValue() {
     return K == ValueKind::Val;
   }
+
   llvm::APInt getValue() {
     if (K != ValueKind::Val) {
       llvm::errs() << "Interpreter: expected number but got ";
@@ -80,30 +81,39 @@ struct EvalValue {
     }
   }
 };
+
 using ValueCache = std::unordered_map<souper::Inst *, EvalValue>;
 
-EvalValue evaluateAddNSW(llvm::APInt a, llvm::APInt b);
-EvalValue evaluateAddNUW(llvm::APInt a, llvm::APInt b);
-EvalValue evaluateAddNW(llvm::APInt a, llvm::APInt b);
-EvalValue evaluateSubNSW(llvm::APInt a, llvm::APInt b);
-EvalValue evaluateSubNUW(llvm::APInt a, llvm::APInt b);
-EvalValue evaluateSubNW(llvm::APInt a, llvm::APInt b);
-EvalValue evaluateUDiv(llvm::APInt a, llvm::APInt b);
-EvalValue evaluateURem(llvm::APInt a, llvm::APInt b);
-EvalValue evaluateShl(llvm::APInt a, llvm::APInt b);
-EvalValue evaluateLShr(llvm::APInt a, llvm::APInt b);
-EvalValue evaluateAShr(llvm::APInt a, llvm::APInt b);
+EvalValue evaluateAddNSW(llvm::APInt A, llvm::APInt B);
+EvalValue evaluateAddNUW(llvm::APInt A, llvm::APInt B);
+EvalValue evaluateAddNW(llvm::APInt A, llvm::APInt B);
+EvalValue evaluateSubNSW(llvm::APInt A, llvm::APInt B);
+EvalValue evaluateSubNUW(llvm::APInt A, llvm::APInt B);
+EvalValue evaluateSubNW(llvm::APInt A, llvm::APInt B);
+EvalValue evaluateUDiv(llvm::APInt A, llvm::APInt B);
+EvalValue evaluateURem(llvm::APInt A, llvm::APInt B);
+EvalValue evaluateShl(llvm::APInt A, llvm::APInt B);
+EvalValue evaluateLShr(llvm::APInt A, llvm::APInt B);
+EvalValue evaluateAShr(llvm::APInt A, llvm::APInt B);
 
-EvalValue evaluateInst(Inst* Root, ValueCache &Cache);
-llvm::KnownBits findKnownBits(Inst* I, ValueCache& C, bool PartialEval = true);
-llvm::KnownBits findKnownBitsUsingSolver(Inst *I, Solver *S, std::vector<InstMapping> &PCs);
-llvm::ConstantRange findConstantRange(souper::Inst* I,
-                                      souper::ValueCache& C,
-                                      bool PartialEval = true);
-llvm::ConstantRange findConstantRangeUsingSolver(souper::Inst* I, Solver *S, std::vector<InstMapping> &PCs);
-bool isConcrete(souper::Inst *I, bool ConsiderConsts = true,
-                                 bool ConsiderHoles = true);
-std::string knownBitsString(llvm::KnownBits KB);
+  class ConcreteInterpreter {
+    ValueCache Cache;
+    bool CacheWritable = false;
+
+    EvalValue evaluateSingleInst(Inst *I, std::vector<EvalValue> &Args);
+
+  public:
+    ConcreteInterpreter() {}
+    ConcreteInterpreter(ValueCache &Input) : Cache(Input) {}
+    ConcreteInterpreter(Inst *I, ValueCache &Input) : Cache(Input) {
+      CacheWritable = true;
+      evaluateInst(I);
+      CacheWritable = false;
+    }
+
+    EvalValue evaluateInst(Inst *Root);
+  };
+
 }
 
 

lib/Infer/Interpreter.cpp
@@ -15,10 +15,6 @@
 #include "souper/Infer/Interpreter.h"
 
 namespace souper {
-#define ARG0 Args[0].getValue()
-#define ARG1 Args[1].getValue()
-#define ARG2 Args[2].getValue()
-
   EvalValue evaluateAddNSW(llvm::APInt a, llvm::APInt b) {
     bool Ov;
     auto Res = a.sadd_ov(b, Ov);
@@ -105,14 +101,18 @@ namespace souper {
     return {a.ashr(b)};
   }
 
-  EvalValue evaluateSingleInst(Inst *Inst, std::vector<EvalValue> &Args) {
+#define ARG0 Args[0].getValue()
+#define ARG1 Args[1].getValue()
+#define ARG2 Args[2].getValue()
+
+  EvalValue ConcreteInterpreter::evaluateSingleInst(Inst *Inst, std::vector<EvalValue> &Args) {
     // UB propagates unconditionally
     for (auto &A : Args)
       if (A.K == EvalValue::ValueKind::UB)
         return EvalValue::ub();
 
     // phi and select only take poison from their chosen input
-    if (Inst->K != Inst::Phi && Inst->K != Inst::Select) {
+    if (Inst->K != Inst::Select) {
       for (auto &A : Args)
         if (A.K == EvalValue::ValueKind::Poison)
           return EvalValue::poison();
@@ -134,7 +134,13 @@ namespace souper {
 
     case Inst::Phi:
       // FIXME should return all values not just one of them
-      return ARG0;
+      // At this moment, the only situation where evaluateInst is called on an
+      // Instcontaining Phi is for the purpose of partial evaluation during
+      // abstract interpretation. In this case, it is okay to return one of the
+      // operands. If we ever want to deterministically interpret an LHS
+      // containing a phi, this needs to start returning a list, or there needs
+      // to be enough information in BlockPCs to interpret ARG0
+      return Args[0];
 
     case Inst::Add:
       return {ARG0 + ARG1};
@@ -287,7 +293,7 @@ namespace souper {
 
     case Inst::Select:
       if (!Args[0].hasValue())
-        return EvalValue::ub();
+        return Args[0];
       return ARG0.getBoolValue() ? Args[1] : Args[2];
 
     case Inst::ZExt:
@@ -375,19 +381,17 @@ namespace souper {
 #undef ARG1
 #undef ARG2
 
-  EvalValue evaluateInst(Inst *Root, ValueCache &Cache) {
-    // TODO populate cache and look things up in it
-    // needed?
-    if (Root->K == Inst::Var) {
+  EvalValue ConcreteInterpreter::evaluateInst(Inst *Root) {
+    if (Cache.find(Root) != Cache.end())
       return Cache[Root];
-    } else {
-      // TODO SmallVector
-      std::vector<EvalValue> EvaluatedArgs;
-      for (auto &&I : Root->Ops)
-        EvaluatedArgs.push_back(evaluateInst(I, Cache));
-      auto Result = evaluateSingleInst(Root, EvaluatedArgs);
-      //Cache[Root] = Result;
-      return Result;
-    }
+
+    // TODO SmallVector
+    std::vector<EvalValue> EvaluatedArgs;
+    for (auto &&I : Root->Ops)
+      EvaluatedArgs.push_back(evaluateInst(I));
+    auto Result = evaluateSingleInst(Root, EvaluatedArgs);
+    if (CacheWritable)
+      Cache[Root] = Result;
+    return Result;
   }
 }